ab6d819328098ed3c212fcdcb6e0888c57d8bfb9
James Stewart <james.stewart.06@aberdeen.ac.uk>
2017-12-07 21:44:36 +0000

    Radio Button changer works

--- public/Table.js
+++ public/Table.js
@@ -1,33 +1,25 @@
 var Table = function(){
-this.statusBoolean();
+  this.statusBoolean();
 }
 
-Table.prototype.monthClicker = function(){
+Table.prototype.monthClicker = function(color){
   const tableCells = document.getElementsByTagName('td');
 
   for(var i=0; i < tableCells.length; i++) {
     tableCells[i].addEventListener("click", function(i){
-        this.style.backgroundColor = "green"
+      this.style.backgroundColor = color
     })
   }
 }
 
 Table.prototype.statusBoolean = function(){
   var faceRadioBtn = document.getElementById('face-radio');
-  faceRadioBtn.addEventListener('change', function(){
-    console.log("James");
-  })
   var heelRadioBtn = document.getElementById('heel-radio');
+
+  faceRadioBtn.addEventListener('change', function(){
+    this.monthClicker('green');
+  }.bind(this))
   heelRadioBtn.addEventListener('change', function(){
-    console.log("Steph");
-  })
-
-
-
-
-  var radioStatusBool = (document.getElementById("face-radio").checked)
-  console.log(radioStatusBool)
-  if (radioStatusBool){
-    console.log("Hello")
-  }
+    this.monthClicker("red");
+  }.bind(this))
 }

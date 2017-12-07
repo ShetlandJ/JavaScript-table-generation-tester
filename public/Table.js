390fb83b3a1ab751907f67926277bef6365a2b92
James Stewart <james.stewart.06@aberdeen.ac.uk>
2017-12-07 21:19:34 +0000

    simplified function

--- public/Table.js
+++ public/Table.js
@@ -6,26 +6,31 @@ Table.prototype.monthClicker = function(){
   const tableCells = document.getElementsByTagName('td');
 
   for(var i=0; i < tableCells.length; i++) {
-    tableCells[i].addEventListener("click", setFaceOrHeel(i));
+    tableCells[i].addEventListener("click", function(i){
+      this.style.backgroundColor = "green"
+    })
+
+    // setFaceOrHeel(i));
   }
 
-  function setFaceOrHeel(i) {
-    return function(){
-      var statusSelector = document.getElementById('face-or-heel');
-      statusSelector.style.visibility = "visible"
-
-      var face = document.getElementById('face');
-      face.addEventListener('click', function(){
-        this.style.backgroundColor = "green";
-        statusSelector.style.visibility = "hidden"
-      }.bind(this));
-
-      var heel = document.getElementById('heel');
-      heel.addEventListener('click', function(){
-        this.style.backgroundColor = "red";
-        statusSelector.style.visibility = "hidden"
-      }.bind(this));
-
-    }
-  };
+  // function setFaceOrHeel(i) {
+  //   return function(){
+  //     var statusSelector = document.getElementById('face-or-heel');
+  //     statusSelector.style.visibility = "visible"
+  //     console.log(tableCells[i])
+  //
+  //     var face = document.getElementById('face');
+  //     face.addEventListener('click', function(){
+  //       tableCells[i].style.backgroundColor = "green";
+  //       statusSelector.style.visibility = "hidden"
+  //     }.bind(this));
+  //
+  //     var heel = document.getElementById('heel');
+  //     heel.addEventListener('click', function(){
+  //       tableCells[i].style.backgroundColor = "red";
+  //       statusSelector.style.visibility = "hidden"
+  //     }.bind(this));
+  //
+  //   }
+  // };
 }

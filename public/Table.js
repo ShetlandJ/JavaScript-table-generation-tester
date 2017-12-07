1866ece1109c6f74da2355768b34caf659250238
James Stewart <james.stewart.06@aberdeen.ac.uk>
2017-12-07 20:57:20 +0000

    colour selector is working, but not entirely as I'd like

--- public/Table.js
+++ public/Table.js
@@ -5,15 +5,27 @@ var Table = function(){
 Table.prototype.monthClicker = function(){
   const tableCells = document.getElementsByTagName('td');
 
-  for(var i=0; i< tableCells.length; i++) {
-        tableCells[i].addEventListener("click", bindClick(i));
+  for(var i=0; i < tableCells.length; i++) {
+    tableCells[i].addEventListener("click", setFaceOrHeel(i));
   }
 
-  function bindClick(i) {
+  function setFaceOrHeel(i) {
     return function(){
       var statusSelector = document.getElementById('face-or-heel');
       statusSelector.style.visibility = "visible"
-      }
-    };
-  }
+
+      var face = document.getElementById('face');
+      face.addEventListener('click', function(){
+        this.style.backgroundColor = "green";
+        statusSelector.style.visibility = "hidden"
+      }.bind(this));
+
+      var heel = document.getElementById('heel');
+      heel.addEventListener('click', function(){
+        this.style.backgroundColor = "red";
+        statusSelector.style.visibility = "hidden"
+      }.bind(this));
+
+    }
+  };
 }

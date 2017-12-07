d5112c1b61b934acaca203d1c65e788987ca53a7
James Stewart <james.stewart.06@aberdeen.ac.uk>
2017-12-07 19:35:12 +0000

    Table is clickable and can change a background colour onclick

--- public/app.js
+++ public/app.js
@@ -1,7 +1,17 @@
 var app = function(){
 
-  
+  var tableCell = document.getElementsByClassName('month-cells');
+  console.log(tableCell);
+  tableCell[0].addEventListener('click', function(){
+    console.log("hello")
+    tableCell[0].style.backgroundColor = "#AA0000";
+  });
+};
 
-}
+
+// var colouriseTableCell = function(){
+//   // var tableCell = document.getElementById('td');
+//   tableCell.style.backgroundColor = "FFFFFF";
+// }
 
 window.addEventListener('load', app);

46f143d59da7eb7816c54b6915269dbbc4ba46f8
James Stewart <james.stewart.06@aberdeen.ac.uk>
2017-12-07 21:38:23 +0000

    Listeners addded to radio buttons

--- public/Table.js
+++ public/Table.js
@@ -1,5 +1,5 @@
 var Table = function(){
-
+this.statusBoolean();
 }
 
 Table.prototype.monthClicker = function(){
@@ -7,9 +7,27 @@ Table.prototype.monthClicker = function(){
 
   for(var i=0; i < tableCells.length; i++) {
     tableCells[i].addEventListener("click", function(i){
-      this.style.backgroundColor = "green"
+        this.style.backgroundColor = "green"
     })
   }
+}
+
+Table.prototype.statusBoolean = function(){
+  var faceRadioBtn = document.getElementById('face-radio');
+  faceRadioBtn.addEventListener('change', function(){
+    console.log("James");
+  })
+  var heelRadioBtn = document.getElementById('heel-radio');
+  heelRadioBtn.addEventListener('change', function(){
+    console.log("Steph");
+  })
 
 
+
+
+  var radioStatusBool = (document.getElementById("face-radio").checked)
+  console.log(radioStatusBool)
+  if (radioStatusBool){
+    console.log("Hello")
+  }
 }
